f97cd430e0533f67ac80ab6995b58080eefd9a44
Josh Shamouil <joshuashamouil@gmail.com>
2023-06-30 12:29:32 -0400

    delete print statements

--- solver_calc.py
+++ solver_calc.py
@@ -22,9 +22,6 @@ def calc(guess, output, words=None):
     for let, i in yellow:
         if let in gray:
             gray.remove(let)
-    print(gray)
-    print(yellow)
-    print(green)
     for word in words:
         out_words.update({word for i in gray if i in word})
         out_words.update({word for let, i in yellow if in_yellow(let, i, word)})
@@ -32,9 +29,3 @@ def calc(guess, output, words=None):
             in_words.add(word)
     words = (words.difference(out_words)).intersection(in_words)
     return words
-
-lw =  ['setup', ' scent', ' fetch', ' guest', ' duvet', ' steel', ' sleet', ' tweed', ' depot', ' covet', ' onset', ' tempo', ' often', ' steep', ' exult', ' unmet', ' token', ' sweet', ' sheet', ' unset', ' event', ' dwelt', ' fleet', ' extol', ' slept', ' swept', ' betel', ' steed', ' elect', ' beget', ' knelt', ' detox', ' upset', ' theft', ' motel', ' debut', ' spent', ' hotel', ' octet', ' comet', ' tweet', ' cleft', ' beset', ' ethos', ' quest', ' towel', ' smelt', ' eject', ' totem', ' teddy', ' tenet', ' fetus', ' chest', ' spelt']
-lw = set(lw)
-print(lw)
-test = calc("onset", "3,3,2,2,1", lw)
-print(test)
